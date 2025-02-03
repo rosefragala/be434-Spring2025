49f1305e9f0f2b3230fa89ca6bbb98451db93c03
Rose Fragala <146766044+rosefragala@users.noreply.github.com>
2025-02-03 01:01:18 +0000

    Homework 1 to the best of my ability.

--- assignments/01_howdy/howdy.py
+++ assignments/01_howdy/howdy.py
@@ -40,17 +40,10 @@ def main():
     """To generate and print the greetings! (well, hopefully)"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
-
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    greeting = args.greeting
+    name = args.name
+    punctuation = '!' if args.excited else '.'
+    print(f'{greeting}, {name}{punctuation}')
 
 
 # --------------------------------------------------
